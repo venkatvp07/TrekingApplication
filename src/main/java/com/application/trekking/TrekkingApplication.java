0fcd2e2864df027866658be802920ab8c93f766e
venkat.prasath <venkat.prasath@zohocorp.com>
2025-04-25 23:17:02 +0530

    Base - spring boot application files added

---
+++ src/main/java/com/application/trekking/TrekkingApplication.java
@@ -0,0 +1,13 @@
+package com.application.trekking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/trekking/TrekkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.trekking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
